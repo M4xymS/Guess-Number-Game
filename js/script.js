b850b9f63bc4449f578450ded2da3b4087be7aeb
Maxym <smolinskimaksym@gmail.com>
2022-05-25 08:57:27 +0200

    quick fix in script

--- js/script.js
+++ js/script.js
@@ -8,23 +8,21 @@ let count = 0;
 const Game = () => {
   const input = document.querySelector("#number").value;
 
-  setTimeout(restartGame, 5000);
+  if (input > 15) {
+    resultField.innerHTML = `Guess number from 1 to 15!`;
+  } else if (input < randomNumber) {
+    resultField.innerHTML = `Your Number is Higher than ${input}!`;
+  } else {
+    resultField.innerHTML = `Your Number is Lower than ${input}!`;
+  }
+  if (input == randomNumber) {
+    resultField.innerHTML = `Wow! u got that! the number is: ${randomNumber}`;
+    setTimeout(restartGame, 5000);
+    const restartGame = () => {
+      location.reload();
+    };
+  }
 };
-if (input > 15) {
-  resultField.innerHTML = `Guess number from 1 to 15!`;
-} else if (input < randomNumber) {
-  resultField.innerHTML = `Your Number is Higher than ${input}!`;
-} else {
-  resultField.innerHTML = `Your Number is Lower than ${input}!`;
-}
-if (input == randomNumber) {
-  resultField.innerHTML = `Wow! u got that! the number is: ${randomNumber}`;
-
-  const restartGame = () => {
-    location.reload();
-  };
-}
-
 const countTries = () => {
   const input = document.querySelector("#number").value;
   if (input > 15 && count == 0) {
