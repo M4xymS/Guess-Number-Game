d736dc8796c9477660ec15d076dfb21ef7e739f8
Maxym <smolinskimaksym@gmail.com>
2022-05-25 08:54:15 +0200

    i made website responsive

--- js/script.js
+++ js/script.js
@@ -7,22 +7,23 @@ let count = 0;
 
 const Game = () => {
   const input = document.querySelector("#number").value;
-  if (input == randomNumber) {
-    resultField.innerHTML = `Wow! u got that! the number is: ${randomNumber}`;
 
-    const restartGame = () => {
-      location.reload();
-    };
-    setTimeout(restartGame, 5000);
-  }
-  if (input > 15) {
-    resultField.innerHTML = `Guess number from 1 to 15!`;
-  } else if (input < randomNumber) {
-    resultField.innerHTML = `Your Number is Higher than ${input}!`;
-  } else {
-    resultField.innerHTML = `Your Number is Lower than ${input}!`;
-  }
+  setTimeout(restartGame, 5000);
 };
+if (input > 15) {
+  resultField.innerHTML = `Guess number from 1 to 15!`;
+} else if (input < randomNumber) {
+  resultField.innerHTML = `Your Number is Higher than ${input}!`;
+} else {
+  resultField.innerHTML = `Your Number is Lower than ${input}!`;
+}
+if (input == randomNumber) {
+  resultField.innerHTML = `Wow! u got that! the number is: ${randomNumber}`;
+
+  const restartGame = () => {
+    location.reload();
+  };
+}
 
 const countTries = () => {
   const input = document.querySelector("#number").value;
